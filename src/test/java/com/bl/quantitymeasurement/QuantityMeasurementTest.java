0cc4367152f7c6b33f90029088afbb45e3cb3016
Manasi2804 <shahmanasi281996@gmail.com>
2020-05-19 15:56:23 +0530

    ADDED:To check equality for value

--- src/test/java/com/bl/quantitymeasurement/QuantityMeasurementTest.java
+++ src/test/java/com/bl/quantitymeasurement/QuantityMeasurementTest.java
@@ -12,14 +12,22 @@ public class QuantityMeasurementTest {
         double value2 = quantityMeasurement.getFeet(0.0);
         Assert.assertEquals(value1, value2, 0.0);
     }
+
     @Test
     public void givenFeetValue_WhenNull_ShouldReturnFalse() {
         boolean NullValue = quantityMeasurement.equals(null);
         Assert.assertEquals(false, NullValue);
     }
+
     @Test
     public void givenReferenceObject_WhenEqual_ShouldReturnTrue() {
         QuantityMeasurement object = new QuantityMeasurement();
         Assert.assertEquals(quantityMeasurement, object);
+    }
+    @Test
+    public void givenTwoEqualFeetValues_WhenEqual_ShouldReturnTrue() {
+        double value1 = quantityMeasurement.getFeet(3.0);
+        double value2 = quantityMeasurement.getFeet(3.0);
+        Assert.assertEquals(value1, value2, 0.0);
     }
 }
