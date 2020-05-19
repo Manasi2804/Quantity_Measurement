c60f040ae971ff7bebaf238c4b5e4ca1864a01ef
Manasi2804 <shahmanasi281996@gmail.com>
2020-05-19 22:25:45 +0530

    ADDED:Test case for null inch value

--- src/test/java/com/bl/quantitymeasurement/QuantityMeasurementTest.java
+++ src/test/java/com/bl/quantitymeasurement/QuantityMeasurementTest.java
@@ -24,22 +24,30 @@ public class QuantityMeasurementTest {
         QuantityMeasurement object = new QuantityMeasurement();
         Assert.assertEquals(quantityMeasurement, object);
     }
+
     @Test
     public void givenTwoEqualFeetValues_WhenEqual_ShouldReturnTrue() {
         double value1 = quantityMeasurement.getFeet(3.0);
         double value2 = quantityMeasurement.getFeet(3.0);
         Assert.assertEquals(value1, value2, 0.0);
     }
+
     @Test
-    public void givenDifferentFeetValues_WhenEqual_ShouldReturnFalse(){
+    public void givenDifferentFeetValues_WhenEqual_ShouldReturnFalse() {
         double value1 = quantityMeasurement.getFeet(0.0);
         double value2 = quantityMeasurement.getFeet(1.0);
         Assert.assertNotEquals(value1, value2, 0.0);
     }
+
     @Test
     public void givenZeroInchAndZeroInchValue_WhenEqual_ShouldReturnTrue() {
         double value1 = quantityMeasurement.getInch(0.0);
         double value2 = quantityMeasurement.getInch(0.0);
         Assert.assertEquals(value1, value2, 0.0);
+    }
+    @Test
+    public void givenInchValue_WhenNull_ShouldReturnFalse() {
+        boolean NullValue = quantityMeasurement.equals(null);
+        Assert.assertEquals(false, NullValue);
     }
 }
