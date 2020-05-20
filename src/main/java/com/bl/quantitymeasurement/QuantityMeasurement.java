b2d9dff6f708bf5ad6be40bedd606ff3499f514d
Manasi2804 <shahmanasi281996@gmail.com>
2020-05-20 23:07:31 +0530

    REFACTORED:Added comments in method and class

--- src/main/java/com/bl/quantitymeasurement/QuantityMeasurement.java
+++ src/main/java/com/bl/quantitymeasurement/QuantityMeasurement.java
@@ -1,8 +1,18 @@
+/*******************************************************************
+ * @purpose:Compare different unit value and return converted value
+ * @Author:Manasi
+ * @Date:-20/5/2020
+ *******************************************************************/
 package com.bl.quantitymeasurement;
 import com.bl.quantitymeasurement.exception.QuantityMeasurementException;
 
 public class QuantityMeasurement{
-
+    /*
+     * @purpose:-calculate conversion
+     * @param units:-value to be converted
+     * @param value:-value which we convert
+     * @return:-converted value
+     */
     public double getUnit(Units units, double value) {
         if (value < 0) {
             throw new QuantityMeasurementException(QuantityMeasurementException.ExceptionType.ENTER_VALUE, "Value Should be Correct");
@@ -10,6 +20,11 @@ public class QuantityMeasurement{
             return value * units.unit;
         }
     }
+    /*
+     * @purpose:-to check equality condition
+     * @param obj:-null and reference
+     * @return:-return true or false
+     */
     @Override
     public boolean equals(Object obj) {
         if (obj==null) return false;
